a0ce0303bc5ef1d0c67c35713d44638abb493d6d
fixer21k <fixer21k@4ef296e0-c894-11de-8f3f-2f730f80caef>
2009-11-09 20:46:00 +0000

    sync with trunk(index.html,stlyle.css)

--- branches/kim/index.php
+++ branches/kim/index.php
@@ -1,7 +1,8 @@
+<!DOCTYPE html>
 <html>
 	<head>
 		<title>Главная страница</title>
-		<meta content ="charset = utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset = UTF-8">
 		<link rel = "stylesheet" type = "text/css" href = "style.css" />
 	</head>
 	<body>
