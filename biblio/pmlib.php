db944b89a7cf5eddbebef184dd9d4c61e1da115b
fixer21k <fixer21k@4ef296e0-c894-11de-8f3f-2f730f80caef>
2009-11-09 19:36:22 +0000

    branch unconfirmed version

---
+++ biblio/pmlib.php
@@ -0,0 +1,32 @@
+<?php
+require_once('mylib.php');
+require_once('logins.php'); /* don't upload this file to svn!!! */
+?>
+<div id = "separator"></div>
+<div id = "vmenu">Дополнительное меню</div>
+<div id = "main" >
+<?php
+$link = mysql_connect("localhost", dbuser, dbpassword);
+
+/*
+ * Не стоит это конечно показывать пользователю, но
+ * пока об этом думать рано, не так ли?
+ */
+if (!$link)
+	die('Could not connect: ' . mysql_error());
+
+if (!mysql_select_db(dbname, $link))
+	die('Could not select db: ' . mysql_error());
+
+$query = "SELECT * from biblio;";
+$resource = mysql_query($query);
+
+if (!$resource) {
+	die('Invalid query: ' . mysql_error());
+}
+
+while ($row = mysql_fetch_assoc($resource))
+	echo make_bookdiv($row);
+
+?>
+</div>
---
+++ main.php
@@ -0,0 +1,7 @@
+<?php
+$simpleout = "<div id = \"separator\"></div>" .
+	     "<div id = \"vmenu\">Дополнительное меню</div>" .
+	     "<div id = \"main\" >Content</div>";
+
+echo $simpleout;
+?>
