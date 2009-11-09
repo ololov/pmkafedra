f7619d845261b165eccc48c401328e8f49dce538
fixer21k <fixer21k@4ef296e0-c894-11de-8f3f-2f730f80caef>
2009-11-09 02:20:25 +0000

    Особо впечатлительным лучше несмотреть пока на библиотеку :). А теперь все попорядку: bibdb.sh - скрипт который создает таблицу в базе данных и пару полей. biblio.php - почти что копия главной страницы, за исключением того, что 		своим видом Вас может напугать. mylib.php - Функции мои для генерирования того, что я пока называю ужасом. :)

---
+++ trunk/biblio.php
@@ -0,0 +1,59 @@
+<?php
+require_once('mylib.php');
+require_once('logins.php'); /* don't upload this file to svn!!! */
+?>
+<html>
+	<head>
+		<title>Главная страница</title>
+		<meta content ="charset = utf-8">
+		<link rel = "stylesheet" type = "text/css" href = "style.css" />
+	</head>
+	<body>
+		<div id = "container">
+			<div id = "head"><h6><span>Московский Государственный Технический Университет Гражданской Авиации</span></h6>
+					 <h1>БИБЛИОТЕКА КАФЕДРЫ ПРИКЛАДНОЙ МАТЕМАТИКИ</h1>
+			</div>
+			<div class = "gmenu">
+				<div class = "gmenucont">
+					<ul  class = "wrpr">
+						<li><a href = "index.html">Информация о кафедре</a></li>
+						<li><a href = "index.html">Студенту</a></li>
+						<li><a href = "index.html">Научная работа</a></li>
+						<li><a href = "index.html">Новости и события</a></li>
+						<li><a href = "#">Библиотека</a></li> 
+						<li><a href = "index.html">Форум</a></li>
+						<li><a href = "index.html">Галерея</a></li>
+					</ul>
+				</div>
+			</div>
+			<div id = "separator"></div>
+			<div id = "vmenu">Дополнительное меню</div>
+			<div id = "main" >
+<?php
+$link = mysql_connect("localhost", dbuser, dbpassword);
+
+/*
+ * Не стоит это конечно показывать пользователю, но
+ * пока об этом думать рано, не так ли?
+ */
+if (!$link)
+	die('Could not connect: ' . mysql_error());
+
+if (!mysql_select_db(dbname, $link))
+	die('Could not select db: ' . mysql_error());
+
+$query = "SELECT * from biblio;";
+$resource = mysql_query($query);
+
+if (!$resource) {
+	die('Invalid query: ' . mysql_error());
+}
+
+while ($row = mysql_fetch_assoc($resource))
+	echo make_bookdiv($row);
+
+?>
+</div>
+		</div>
+	</body>
+</html>
