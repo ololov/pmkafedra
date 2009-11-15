85a5105087da4de9de0ffcf064e6ef8564441d90
wildbear07 <wildbear07@4ef296e0-c894-11de-8f3f-2f730f80caef>
2009-11-15 22:55:00 +0000

    Разбавил намного парсер коментами и немного подправил код. Прменил его ко всему расписанию касающегося нашего факультета, все полученные файлы лежат в папке parser_pm

--- schedule/preparation.php
+++ schedule/preparation.php
@@ -1,27 +1,40 @@
 ﻿<?php
 	/**
-	*Это скрипт еще не парсер, он тольок подготавливает исходный 
-	*xml документ к виду пригодному для парсинга =) 
+	* Это скрипт еще не парсер, он тольок подготавливает исходный 
+	* xml документ к виду пригодному для парсинга =) 
+	* Кто видел исходный xml документ,наверняка заметил, что не везде есть отдельный тег
+	* для даты, в которую должен идти тот или иной предмет. Например:
+	* <Data ss:Type="String">Ивенин И.Б., доц., только 01.09,08.09</Data>
+	* Поэтому надо разделить этот тег на два тега, для этой цели и написан этот файл
+	* =))
+	* 
 	*/
-    define("XML_FILE",'1234.xml');
+	define("XML_FILE",'1234.xml');
+	define("TXT_FILE",'para_plus_predmet.txt');
 
-    $dom = new DomDocument;
-    $dom->preserveWhiteSpace = false;
-    $dom->load(XML_FILE);
+	$dom = new DomDocument;
+	$dom->preserveWhiteSpace = false;
+	$dom->load(XML_FILE);
        
-    $domxpath = new domxpath($dom);
-    $domxpath->registerNamespace("m","urn:schemas-microsoft-com:office:spreadsheet");
-           
-    $query = "/m:Workbook/m:Worksheet/m:Table/m:Row/m:Cell[@m:StyleID='ce12']/m:Data";
-    $query2 = "/m:Workbook/m:Worksheet/m:Table/m:Row/m:Cell[@m:StyleID='ce2']/m:Data | /m:Workbook/m:Worksheet/m:Table/m:Row/m:Cell[@m:StyleID='ce10']/m:Data";
+	$domxpath = new domxpath($dom);
+	$domxpath->registerNamespace("m","urn:schemas-microsoft-com:office:spreadsheet");
+        /**
+	* Формирование двух запросов, первый ищет сторки с ФИО преподавателя,
+	* второй ищет пары + название предмета
+	*/
+	$query = "/m:Workbook/m:Worksheet/m:Table/m:Row/m:Cell[@m:StyleID='ce12']/m:Data";
+	$query2 = "/m:Workbook/m:Worksheet/m:Table/m:Row/m:Cell[@m:StyleID='ce2']/m:Data | /m:Workbook/m:Worksheet/m:Table/m:Row/m:Cell[@m:StyleID='ce10']/m:Data";
     
-    $str_parse = "только";
-    $filename = "2.txt";
-    $datas = $domxpath->query($query);
-    $datas2 = $domxpath->query($query2);
+    	$str_parse = "только";
+    	$datas = $domxpath->query($query);
+	$datas2 = $domxpath->query($query2);
     
-    foreach($datas as $data){
-    	if(stripos($data->nodeValue,$str_parse)){
+    	/**
+	* Здесь происходит добавление в xml документ отдельного тега для
+	* даты
+	*/
+	foreach($datas as $data){
+    		if(stripos($data->nodeValue,$str_parse)){
 			list($str1, $str2) = explode($str_parse,$data->nodeValue);
 			$str1[strlen($str1)-2] = "";
 			$str2 = $str_parse.$str2;
@@ -39,25 +52,39 @@
 			$data->nodeValue = $str1;
 			
 			$dom->formatOutput = true;
+    		}
     	}
-    }
-
-    if(is_writeable($filename)){
-    	$fl = fopen($filename, "a+");
-    	foreach($datas2 as $data){
+	/**
+	* Формирование сторки вида <номер пары>:<предмет>
+	* и запись этой строки в файл
+	*/
+   	$fl = fopen(TXT_FILE, "w+");
+	foreach($datas2 as $data){
     		$dt = $data->nodeValue;
-  			if(is_numeric($dt)){
-  				$para = $dt.":";
-  				continue;
-  			}
-  			if(is_string($dt)){
-  				if(isset($para)){
-  					$str = $para.$dt.":\n";
-  				}
-  				fwrite($fl,$str);
-  			}	  	
+  		if(is_numeric($dt)){
+  			$para = $dt.":";
+  			continue;
+  		}
+  		if(is_string($dt)){
+  			if(isset($para)) $str = $para.$dt."\n";
+  			fwrite($fl,$str);
+			/**
+			* Присвоение предмету Физ-ра преподавателся "Неизвестен" =))
+			* 
+			*/
+			if($dt === "Физическая культура"){
+				$parent = $data->parentNode->parentNode->nextSibling->firstChild;
+						
+				$new_data = $dom->createElement('Data',"Неизвестен");
+				$new_data->setAttribute("ss:Type","String");
+				$parent->appendChild($new_data);
+			
+				$dom->formatOutput = true;
+			}
+
+  		}	  	
     	}
     	fclose($fl);
-    }else echo "Файл $filename не создан \n";   
-    echo $dom->saveXML();
+
+    	echo $dom->saveXML();
 ?> 
