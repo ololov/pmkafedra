ab67c685f2362a730330ac7293fc17113e128452
wildbear07 <wildbear07@4ef296e0-c894-11de-8f3f-2f730f80caef>
2009-11-15 22:55:00 +0000

    Разбавил намного парсер коментами и немного подправил код. Прменил его ко всему расписанию касающегося нашего факультета, все полученные файлы лежат в папке parser_pm

--- trunk/schedule/parser.php
+++ trunk/schedule/parser.php
@@ -1,34 +1,37 @@
 <?php
-	 define("XML_FILE",'temp.xml');
-
-    $dom = new DomDocument;
-    $dom->preserveWhiteSpace = false;
-    $dom->load(XML_FILE);
+	//Обьявление констант XML_FILE & TXT_FILE
+	define("XML_FILE",'temp.xml');
+	define("TXT_FILE",'para_plus_predmet.txt');
+	//
+    	$dom = new DomDocument;
+	$dom->preserveWhiteSpace = false;
+	$dom->load(XML_FILE);
        
-    $domxpath = new domxpath($dom);
-    $domxpath->registerNamespace("m","urn:schemas-microsoft-com:office:spreadsheet");
-    $query = "/m:Workbook/m:Worksheet/m:Table/m:Row/m:Cell[@m:StyleID='ce12']/m:Data | /m:Workbook/m:Worksheet/m:Table/m:Row/m:Cell[@m:StyleID='ce11']/m:Data | /m:Workbook/m:Worksheet/m:Table/m:Row/m:Cell[@m:StyleID='ce13']/m:Data | /m:Workbook/m:Worksheet/m:Table/m:Row/m:Cell[@m:StyleID='ce3']/m:Data";
-    
-    $datas = $domxpath->query($query);
-    $filename = "result.txt";
-    $filename2 = "2.txt";
-	if(is_writeable($filename) && is_file($filename2)){
-		$fl = fopen($filename, "a+");
-		$fl_array = file($filename2);
-		$j = $k = 0;
-    	for($i = 0; $i < $datas->length; $i++){
-    		$dt = trim($datas->item($i)->nodeValue);
-    		$fl_array[$j] = trim($fl_array[$j],"\n");
-    		$fl_array[$j] .= $dt.":";$k++;
-    		if($k % 4 === 0){
-    			$fl_array[$j] .= "\n";
-    			fwrite($fl,$fl_array[$j]);
-    			$j++;
-    		}
-    		
-    	}
+	
+	$domxpath = new domxpath($dom);
+	$domxpath->registerNamespace("m","urn:schemas-microsoft-com:office:spreadsheet");
+	/**
+	* Формирование запроса для поиска по xml файлу,результатом будет 
+	* тип(лекция, практика и тп), преподватель, аудитория, дата
+	*/
+	$query = "/m:Workbook/m:Worksheet/m:Table/m:Row/m:Cell[@m:StyleID='ce12']/m:Data | /m:Workbook/m:Worksheet/m:Table/m:Row/m:Cell[@m:StyleID='ce11']/m:Data | /m:Workbook/m:Worksheet/m:Table/m:Row/m:Cell[@m:StyleID='ce13']/m:Data | /m:Workbook/m:Worksheet/m:Table/m:Row/m:Cell[@m:StyleID='ce3']/m:Data";
 
-    	fclose($fl);
-    }else echo "Проверьте существование файла 2.txt \n";
- 
+    	$datas = $domxpath->query($query);
+
+	/**
+	* Формирование строки вида 
+	* <номер пары>:<предмет>:<преподаватель>:<аудитория>:<дата>
+	*/
+
+	if(is_file(TXT_FILE)){
+		$fl_array = file(TXT_FILE);
+		for($i = $j = $k = 0; $i < $datas->length; $i++){
+			$dt = trim($datas->item($i)->nodeValue);
+			$fl_array[$j] = trim($fl_array[$j],"\n").":".$dt;$k++;
+			if($k % 4 === 0){
+				echo $fl_array[$j]."\n";
+				$j++;
+			}
+		}
+	}else echo "Проверьте существование файла ".TXT_FILE."\n";
 ?>
