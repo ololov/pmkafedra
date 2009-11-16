f8b745f3295a31120e5bd53f1e7b6a32588294b3
kulickova <kulickova@4ef296e0-c894-11de-8f3f-2f730f80caef>
2009-11-16 00:38:54 +0000

    Добавила то, что сделала Маша; немного изменила блок <Информация о кафедре>;

---
+++ info/sidebar.php
@@ -0,0 +1,8 @@
+<div id = "vmenu">
+	<p>Навигация</p>
+	<ul>	
+		<li><a href="?page=info"   >Информация о кафедре</a></li>
+		<li><a href="?page=staff"  >Сотрудники кафедры  </a></li>
+		<li><a href="?page=contact">Контакты</a></li>
+	</ul>
+</div>
