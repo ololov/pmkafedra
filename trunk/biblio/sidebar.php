4c27711a7082c3171432b20dcbb9ea97c8a430d9
fixer21k <fixer21k@4ef296e0-c894-11de-8f3f-2f730f80caef>
2009-11-16 11:14:58 +0000

    забыл вставить файл pmlib.php

--- trunk/biblio/sidebar.php
+++
@@ -1,8 +0,0 @@
-<div id = "vmenu">
-	<p>Навигация</p>
-	<ul>	
-		<li><a href="?page=pmlib&view=list"   >Список книг</a></li>
-		<li><a href="?page=pmlib&view=search"  >Поиск книги</a></li>
-		<li><a href="?page=pmlib&view=add">Добавить книгу</a></li>
-	</ul>
-</div>
