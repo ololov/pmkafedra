0cf8b58a7cdce251dae89192934434da9c1fe5f3
nameless.twice <nameless.twice@4ef296e0-c894-11de-8f3f-2f730f80caef>
2009-11-22 15:03:51 +0000

    Добавлена базовая аутентификация. Используются сессии, кукисы пока не используются. Логин/пароль для доступа web/web. Если кого-то напрягает необходимость авторизации при тестировании, можно добавить свою страничку в список страниц, для которых не нужна авторизация (см. начало файла index.php)

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+
+	include_once("auth/authlib.php");
+	
+	$no_login_needed = array('/index.php?page=login');
+	if (!in_array($_SERVER['REQUEST_URI'], $no_login_needed))
+	{
+		Validator::redirect();
+	}
+	
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -28,17 +40,24 @@
 			<div id = "separator"></div>
 		</div>
 <?php
-/*
- * $scripts - Хранит имена файлов соответствующие запрощенной страницы.
- * 		А именно то что передается из $_GET['page'] явл-ся ключом, а значение это имя файла(скрипта).
- */
-$scripts = array('pmlib' => 'biblio/pmlib.php', 'info' => 'info/info.php', 'staff' => 'info/staff.php', 'contact' => 'info/contact.php', 'list' => 'biblio/list.php', 'upload'=>'biblio/upload_form.php');
-$page = $_GET['page'];
 
-if (isset($page) && array_key_exists($page, $scripts))
-	include_once($scripts[$page]);
-else
-	include_once("main.php");
+	/*
+	 * $scripts - Хранит имена файлов соответствующие запрощенной страницы.
+	 * 		А именно то что передается из $_GET['page'] явл-ся ключом, а значение это имя файла(скрипта).
+	 */
+	$scripts = array('pmlib' => 'biblio/pmlib.php', 
+					 'info' => 'info/info.php', 
+					 'staff' => 'info/staff.php', 
+					 'contact' => 'info/contact.php', 
+					 'list' => 'biblio/list.php', 
+					 'upload' => 'biblio/upload_form.php',
+					 'login' => 'login.php');
+	$page = $_GET['page'];
+
+	if (isset($page) && array_key_exists($page, $scripts))
+		include_once($scripts[$page]);
+	else
+		include_once("main.php");
 
 ?>
 	
