e2c9bfbaa1a631fa184cb6e09df67bc42aa73fd3
nameless.twice <nameless.twice@4ef296e0-c894-11de-8f3f-2f730f80caef>
2009-11-22 15:04:58 +0000

    Вдогонку, забыл добавить форму для входа :)

---
+++ trunk/login.php
@@ -0,0 +1,20 @@
+
+
+<div id = "vmenu"> 
+	<form method="POST" action = "auth/auth.php">
+		Логин:<br>
+		<input type="text" name="login" />
+		Пароль:<br> 
+		<input type="text" name="pwd" /><br>
+		<input type="submit" name="submit" value="Вход">
+	</form>
+</div>
+
+<div id = "main">
+	Пожалуйста, авторизуйтесь!<br>
+	<br>
+	<p>
+	Ваш логин и пароль совпадают с Вашими учетными данными для входа в систему... Шютка =)<br>
+	На самом деле для логина используйте логин "web" с паролем "web" =)
+	</p>
+</div>
