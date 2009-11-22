a5e7b2d9e90785deb2c38b5ac6d6fa1c35e54bf5
kulickova <kulickova@4ef296e0-c894-11de-8f3f-2f730f80caef>
2009-11-22 22:58:26 +0000

    Таблицы к форуму :)

---
+++ trunk/forum/teams.php
@@ -0,0 +1,37 @@
+<div id = forum_main>
+	<table id = "forum">
+		<tr class = "head_forum">
+			<td class = "first">Тема</td>
+			<td>Автор</td>
+			<td>Ответов</td>
+			<td class = "last">Последний ответ</td>
+		</tr>
+		<tr>
+			<td>
+				<img src = "images/topic1.png" class = "img_forum">
+				<a href = "" class = "team">Тема 1</a>
+			</td>
+			<td>Ifght</td>
+			<td>63</td>
+			<td>12.11.09</td>
+		</tr>
+		<tr>
+			<td>
+				<img src = "images/topic2.png" class = "img_forum">
+				<a href = "" class = "team">Тема 2</a>
+			</td>
+			<td>frtr</td>
+			<td>8</td>
+			<td>11.10.09</td>
+		</tr>
+		<tr>
+			<td>
+				<img src = "images/topic3.png" class = "img_forum">
+				<a href = "" class = "team">Тема 3</a>
+			</td>
+			<td>Alex</td>
+			<td>11</td>
+			<td>23.11.09</td>
+		</tr>
+	</table>
+</div>
