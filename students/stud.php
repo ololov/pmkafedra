de9499411a136c3330306f1b3e019a644ea658f7
kulickova <kulickova@4ef296e0-c894-11de-8f3f-2f730f80caef>
2009-12-06 20:55:02 +0000

    Предметы

--- students/stud.php
+++ students/stud.php
@@ -2,7 +2,8 @@
 include('./students/s_sidebar.php');
 
 $deps = array('gos' => './students/disc/discipl.php',
-	      'sched' => './students/schedule/schedule.php');
+	      'sched' => './students/schedule/schedule.php',
+	      'disc'  => './students/classes.php');
 
 if (isset($_GET['dep']) && array_key_exists($_GET['dep'], $deps))
 	include_once($deps[$_GET['dep']]);
