09bb2e9a650def613fb8bb5ff0f2e008e013e1a7
blood-mary <blood-mary@inbox.ru>
2009-12-06 20:05:39 +0000

    зыбыл добавить боковое меню

--- students/s_sidebar.php
+++ students/s_sidebar.php
@@ -1,13 +1,13 @@
 <div id = "vmenu">
 	<p>Навигация</p>
 	<ul>	
-		<li><a href="#">Расписание занятий (+)</a></li>
+		<li><a href="?page=stud&amp;dep=sched&amp;course=1">Расписание занятий (+)</a></li>
 			<ul id = "menulink">
-				<li><a href = "?page=stud&amp;dep=sched">Курс 1</a></li>
-				<li><a href = "?page=stud&amp;dep=sched">Курс 2</a></li>
-				<li><a href = "?page=stud&amp;dep=sched">Курс 3</a></li>
-				<li><a href = "?page=stud&amp;dep=sched">Курс 4</a></li>
-				<li><a href = "?page=stud&amp;dep=sched">Курс 5</a></li>
+				<li><a href = "?page=stud&amp;dep=sched&amp;course=1">Курс 1</a></li>
+				<li><a href = "?page=stud&amp;dep=sched&amp;course=2">Курс 2</a></li>
+				<li><a href = "?page=stud&amp;dep=sched&amp;course=3">Курс 3</a></li>
+				<li><a href = "?page=stud&amp;dep=sched&amp;course=4">Курс 4</a></li>
+				<li><a href = "?page=stud&amp;dep=sched&amp;course=5">Курс 5</a></li>
 			</ul>
 		<li><a href="?page=stud&amp;dep=disc">Предметы</a></li>
 		<li><a href="?page=stud&amp;dep=diplom">Диплом</a></li>
