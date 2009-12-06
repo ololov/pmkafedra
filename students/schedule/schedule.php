e423b101743dbd5a027ee04c865336d47eef703b
wildbear07 <wildbear07@4ef296e0-c894-11de-8f3f-2f730f80caef>
2009-12-06 19:03:14 +0000

    Выкладываю более менее рабочую версию скрипта отвечающего за рассписание, я немного ипользовал javascript для того чтобы обыграть реакцию на пару кнопок, по иному я никак не придумал =))

--- students/schedule/schedule.php
+++ students/schedule/schedule.php
@@ -1,6 +1,10 @@
 <?php 
 	include_once("./students/s_sidebar.php"); 
 
+	/*
+	* Получение номера курса и группы
+	* По умолчанию будет выводиться первый курс и первая группа
+	*/
 	if(isset($_GET['course'])){
 		$course = pg_escape_string($_GET['course']);
 	}else{
@@ -12,17 +16,26 @@
 	}else{
 		$group = 1;
 	}
-
+	/*
+	* 
+	*/
 	$db = pg_pconnect("dbname=clericsu_pm host=localhost user=postgres") or die(pg_last_error());
 	date_default_timezone_set('Europe/Moscow');
 
+	/*
+	* Получение даты начала недели и конца недели по номеру недели в году
+	* просвещенные знают зачем она нужна =))
+	*/
 	function get_interval_week($num_week,$db){
 		$cur_date = strftime("%U", strtotime("$num_week week"));
 		$res = pg_query($db,"SELECT get_interval_week(".date('Y').",$cur_date)");
 		$result = pg_fetch_row($res);
 		return explode("---", $result[0]);
 	}
-	function get_data($db, $num_week, $group){
+	/*
+	* Плучение данных из БД удовлетворяющие запросу
+	*/
+	function get_data($db, $num_week=0, $group){
 		$interval = get_interval_week($num_week,$db);
 		$query = "SELECT para, predmet, lname, fname, sname, ttype, ddate, auditoriya, ggroup FROM schedule_table WHERE ddate >= '".$interval[0]."' AND ddate <= '".$interval[1]."' AND  ggroup = '".$group."' ORDER BY ddate";
 		$result = pg_query($db,$query) or die(pg_last_error());
@@ -30,19 +43,64 @@
 	}
 
 	$group = $course."-".$group;
-	$data = get_data($db,0,$group);
-	$date = array();
-	foreach($data as $dt){
-		if(in_array($dt['ddate'],$date)) continue;
-		else $date[] = $dt['ddate'];
-	}
-	pg_close($db);
 ?>
+<script language = "javascript">
+	/*
+	* Запись чего либо в cookie, в данном срипте запись номера недели, счет идет
+	* от текущей недели года.
+	*/
+	function setCookie(name, value, path, domain, expires, secure){
+		var today = new Date();
+		today.setTime(today.getTime());
+
+	        if(expires){
+			 expires = expires * 1000 * 60 * 60 * 24;
+		}
+		var expires_date = new Date( today.getTime() + (expires) );
+		document.cookie = name + "=" +escape( value ) + ( ( expires ) ? ";expires=" + expires_date.toGMTString() : "" ) + ( ( path ) ? ";path=" + path : "" ) + ( ( domain ) ? ";domain=" + domain : "" ) + ( ( secure ) ? ";secure" : "" );
+
+	}
+	/*
+	* Получение данных из cookie
+	*/
+	function getCookie(name){
+		var cook = document.cookie;
+		var pos = cook.indexOf(name + '=');
+		if(pos == -1){
+			return null;
+		} else {
+			var pos2 = cook.indexOf(';', pos);
+			if(pos2 == -1)
+				return unescape(cook.substring(pos + name.length + 1));
+			else 
+				return unescape(cook.substring(pos + name.length + 1, pos2));
+		}
+	}
+
+	/*
+	* Собственно функции отвечающие за реакцию на нажатии кнопок
+	*/
+	function Next(){
+		var week = getCookie('week');
+		week++;
+		setCookie('week',week);
+		document.myform.submit();
+	}
+	function Last(){
+		var week = getCookie('week');
+		week--;
+		setCookie('week',week);
+		document.myform.submit();
+	}
+</script>
 
 <div id = "main">
 	<p class = "tit"> Расписание занятий </p>
-	<input type="button" style="position:absolute; top:200px; left:300px; height: 20px; width: 100px" value="last week"></input>
-	<input type="button" style="position:absolute; top:200px; right:100px; height: 20px; width: 100px" value="next week"></input>
+	<form name="myform" method="POST">
+		<input type="hidden" name="week" value="">
+		<input type="button" style="position:absolute; top:21%; left:30%; height: 20px; width: 100px" value="last week" onClick = "Last()">
+		<input type="button" style="position:absolute; top:21%; right:10%; height: 20px; width: 100px" value="next week" onClick = "Next()">
+	</form>
 
 
 	<div id = "group">
@@ -55,7 +113,22 @@
 	<div id = "sch">
 		<table id = "schedule">
 		<?php
-			$DaysOfWeek = array("Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"); 
+			$DaysOfWeek = array("Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота");
+			if(isset($_COOKIE['week'])){
+				$week = $_COOKIE['week'];
+			}else{
+				setcookie('week', 0);
+			}
+			$data = get_data($db,$week,$group);
+			/*
+			* Извлечение из полученных данных уникальных дат
+			*/
+			$date = array();
+			foreach($data as $dt){
+				if(in_array($dt['ddate'],$date)) continue;
+				else $date[] = $dt['ddate'];
+			}
+
 			for ($j = 0; $j < count($date); $j++) {
 				$num_day = strftime("%w",strtotime($date[$j]));
 				echo "<td id = sch_day colspan = 2> $DaysOfWeek[$num_day] </td>";
@@ -72,6 +145,7 @@
 					}
 				}
 			}
+			pg_close($db);
 		?>
 		</table>
 	</div>
