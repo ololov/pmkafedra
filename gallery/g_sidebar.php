a6319b44f1c21e59ab24c8917686f0b257021916
kulickova <kulickova@4ef296e0-c894-11de-8f3f-2f730f80caef>
2009-12-13 22:52:56 +0000

    Вдогонку к галерее (фотографии не высылаю: их слишком много :))

---
+++ gallery/g_sidebar.php
@@ -0,0 +1,12 @@
+<div id = "vmenu">
+	<p>Навигация</p>
+	<ul>	
+		<li><a href="?page=gallery&g=study">Мы учимся</a></li>
+		<li><a href="?page=gallery&g=aleksandrov">Александров 2005</a></li>
+		<li><a href="?page=gallery&g=ekvator">Экватор 2008</a></li>
+		<li><a href="?page=gallery&g=kafedra">День кафедры</a></li>
+		<li><a href="?page=gallery&g=maks">Макс 2009</a></li>
+		<li><a href="?page=gallery&g=newyear">Новый год</a></li>
+		<li><a href="?page=gallery&g=peinball">Пейнбол 2008</a>
+	</ul>
+</div>
