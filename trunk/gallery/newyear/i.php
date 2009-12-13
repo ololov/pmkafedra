cb1668ee53e00f124cd24b90e08a2c32db612138
kulickova <kulickova@4ef296e0-c894-11de-8f3f-2f730f80caef>
2009-12-13 22:52:56 +0000

    Вдогонку к галерее (фотографии не высылаю: их слишком много :))

---
+++ trunk/gallery/newyear/i.php
@@ -0,0 +1,25 @@
+<div id = 'main'>
+<script language="JavaScript">
+	var cnt = 28;
+ 	var scr = 9;
+ 	var w   = 200;
+ 	var h   = 150;
+
+ 	document.writeln ('<form>');
+ 		var p = Math.floor(cnt/scr); if (cnt%scr>0) p++;
+ 		for (var i=1;i<=p;i++)
+  			document.write ('<input type=radio onClick="javascript:load('+((i-1)*scr+1)+')">'+i+' ');
+ 	document.writeln ('</form>');
+ 	
+	for (var i=1; i<=scr; i++)
+  		document.writeln ('<img src="void.jpg" name=p'+i+' hspace=4 vspace=4'+(w>0?' width='+w:'')+(h>0?' height='+h:'')+'>');
+ 
+ 	function load (st) {
+  		for (var i=1; i<=scr; i++) {
+   			document.images['p'+i].src= (st>cnt?'void':'gallery/newyear/'+st)+'.jpg';
+   			st++;
+  		}
+ 	}
+ 	load (1);
+</script>
+</div>
