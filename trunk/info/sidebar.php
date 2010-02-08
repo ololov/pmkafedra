237f3f2c106a3fbe5a7fd790ed504d1361e768b7
fixer21k <fixer21k@4ef296e0-c894-11de-8f3f-2f730f80caef>
2010-02-08 11:10:00 +0000

    Удаляю старую версию

--- trunk/info/sidebar.php
+++
@@ -1,8 +0,0 @@
-<div id = "vmenu">
-	<p>Навигация</p>
-	<ul>	
-		<li><a href="?page=info"   >Информация о кафедре</a></li>
-		<li><a href="?page=staff"  >Сотрудники кафедры  </a></li>
-		<li><a href="?page=contact">Контакты</a></li>
-	</ul>
-</div>
