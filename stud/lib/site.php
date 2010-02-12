10708fc5b561c7eccd88aa9621d2d40d00feb641
wildbear07 <wildbear07@4ef296e0-c894-11de-8f3f-2f730f80caef>
2010-02-12 18:19:44 +0000

    А вот и само расписание на все курсы =))

--- stud/lib/site.php
+++ stud/lib/site.php
@@ -9,11 +9,11 @@ function print_sidebar()
 	<ul>	
 		<li><a href="">Расписание занятий (+)</a></li>
 			<ul id = "menulink">
-				<li><a href = "">Курс 1</a></li>
-				<li><a href = "">Курс 2</a></li>
-				<li><a href = "">Курс 3</a></li>
-				<li><a href = "">Курс 4</a></li>
-				<li><a href = "">Курс 5</a></li>
+				<li><a href = "<?php echo stud_url . "/schedule.php?course=1&group=1" ?>">Курс 1</a></li>
+				<li><a href = "<?php echo stud_url . "/schedule.php?course=2&group=1" ?>">Курс 2</a></li>
+				<li><a href = "<?php echo stud_url . "/schedule.php?course=3&group=1" ?>">Курс 3</a></li>
+				<li><a href = "<?php echo stud_url . "/schedule.php?course=4&group=1" ?>">Курс 4</a></li>
+				<li><a href = "<?php echo stud_url . "/schedule.php?course=5&group=1" ?>">Курс 5</a></li>
 			</ul>
 			<li><a href="<?php echo stud_url . "/disc.php";?>">Предметы</a></li>
 		<li><a href="">Диплом</a></li>
