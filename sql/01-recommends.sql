9741ff3423e265e14cff58f6ba63978bb87734dc
fixer21k <fixer21k@4ef296e0-c894-11de-8f3f-2f730f80caef>
2010-02-12 00:11:21 +0000

    Реализовал поддержку рекомендаций. Сырой вывод их уже есть. Осталось за малым! Работа дизайнера и отладка :).
    Добавил функцию path_worker_photo(). Пользуйтесь ею если нужно отобразить фото
    сотрудника кафедры. Дабы если что не пришлось все ссылки перебивать.

--- sql/01-recommends.sql
+++ sql/01-recommends.sql
@@ -55,4 +55,44 @@ EXCEPTION
 END;
 $$ LANGUAGE plPGSQL;
 
+--
+--
+--
+
+DROP TABLE IF EXISTS bd_relation CASCADE;
+CREATE TABLE bd_relation (
+	id_book INTEGER REFERENCES books(id),
+	dname VARCHAR NOT NULL REFERENCES disciplines(name),
+	UNIQUE(id_book, dname)
+);
+
+DROP VIEW IF EXISTS bd_tb CASCADE;
+CREATE VIEW bd_tb AS
+SELECT id_book AS book_id, dname AS disc_name FROM bd_relation;
+
+
+DROP FUNCTION IF EXISTS ADDREC(IN books.id%TYPE, IN workers.ulogin%TYPE, IN recs.record_text%TYPE, IN VARCHAR[]);
+CREATE FUNCTION ADDREC(IN books.id%TYPE, IN workers.ulogin%TYPE, IN recs.record_text%TYPE, IN VARCHAR[])
+RETURNS INTEGER AS $$
+DECLARE ret INTEGER;
+	dset VARCHAR[];
+BEGIN
+	SELECT ADDREC($1, $2, $3) INTO ret;
+
+	SELECT ARRAY(
+		SELECT disc_name FROM disc_tb WHERE disc_name NOT IN(
+			SELECT disc_name FROM bd_tb WHERE book_id = $1) AND disc_name = ANY($4)) INTO dset;
+	IF NOT (array_length(dset, 1) = 0) THEN
+		FOR i IN array_lower(dset, 1) .. array_upper(dset, 1) LOOP
+			INSERT INTO bd_relation(id_book, dname)
+			VALUES ($1, dset[i]);
+		END LOOP;
+	END IF;
+	RETURN ret;
+EXCEPTION
+	WHEN foreign_key_violation THEN
+		RAISE EXCEPTION 'Нет такой книги/дисциплины';
+END;
+$$ LANGUAGE plPGSQL;
+
 
