21679271b074cc4eea28b4ebaa4202f8d53226d2
wildbear07 <wildbear07@4ef296e0-c894-11de-8f3f-2f730f80caef>
2010-02-13 00:08:38 +0000

    Добавил немного красивостей для бокового меню, раздел 'Студенту' =))

--- stud/lib/site.php
+++ stud/lib/site.php
@@ -4,10 +4,23 @@ include_once('include/site.php');
 function print_sidebar()
 {
 ?>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$("#vmenu ul li").hover(function(){
+			$(this).find('ul').animate({height: 'show', width: 'show', opacity: 'show'},'show');
+		}, function(){
+			$(this).find('ul').animate({height: 'hide', width: 'hide', opacity: 'hide'},'hide');
+hide();
+		})
+	})
+</script>
+
 <div id = "vmenu">
 	<p>Навигация</p>
 	<ul>	
-		<li><a href="">Расписание занятий (+)</a></li>
+		<li><a href="">Расписание занятий (+)</a>
+		
 			<ul id = "menulink">
 				<li><a href = "<?php echo stud_url . "/schedule.php?course=1&group=1" ?>">Курс 1</a></li>
 				<li><a href = "<?php echo stud_url . "/schedule.php?course=2&group=1" ?>">Курс 2</a></li>
@@ -15,7 +28,8 @@ function print_sidebar()
 				<li><a href = "<?php echo stud_url . "/schedule.php?course=4&group=1" ?>">Курс 4</a></li>
 				<li><a href = "<?php echo stud_url . "/schedule.php?course=5&group=1" ?>">Курс 5</a></li>
 			</ul>
-			<li><a href="<?php echo stud_url . "/disc.php";?>">Предметы</a></li>
+		</li>
+		<li><a href="<?php echo stud_url . "/disc.php";?>">Предметы</a></li>
 		<li><a href="">Диплом</a></li>
 		<li><a href="">Государственные экзамены</a></li>
 	</ul>
