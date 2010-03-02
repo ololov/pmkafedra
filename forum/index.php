5dc10b9099d2d94195a3c4ce7e97fd3df50f31a0
fixer21k <fixer21k@4ef296e0-c894-11de-8f3f-2f730f80caef>
2010-03-02 15:52:47 +0000

    Добавил страницы "заглушки" в форуме и научной работе.

---
+++ forum/index.php
@@ -0,0 +1,23 @@
+<?php
+include_once('include/site.php');
+?>
+<!DOCTYPE html>
+<html>
+<?php
+print_head("Форум");
+?>
+<body>
+<?php
+print_header();
+?>
+<div id = "vmenu">
+</div>
+<div id = "<?php echo css_content_div; ?>">
+<div id = "umsg">
+Раздел находится в стадии разработки
+</div>
+</div>
+</div>
+</body>
+</html>
+<?php exit; ?>
