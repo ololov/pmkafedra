c15d90cdda88a90d64bfdb52abcba29a8cd7cec8
kulickova <kulickova@4ef296e0-c894-11de-8f3f-2f730f80caef>
2010-03-04 07:12:36 +0000

    Новости, немного поправила оформление таблиц, чуть изменила главную страницу, список предметов

--- trunk/index.php
+++ trunk/index.php
@@ -1,24 +1,61 @@
 <?php
-include_once('include/auth.php');
-init_logins();
+	include_once('include/auth.php');
+	init_logins();
 
-include_once('include/site.php');
+	include_once('include/site.php');
+	include_once('include/lib.php');
+
+	$link = db_connect();
+	if (!$link)
+		include_once('include/html_db_error.php');
+
+	$sql = "SELECT news_text, news_header, news_date FROM news_tb ORDER BY news_date LIMIT 3;";
+
+	$res = pg_query($link, $sql);
+	if (!$res)
+		include_once('include/html_db_error.php');
 ?>
+
 <!DOCTYPE html>
 <html>
-<?php
-print_head("Главная страница");
-?>
-<body>
-<?php
-print_header();
-?>
-<div id = "vmenu">
-<?php
-print_login_form();
-?>
-</div>
-<div id = "<?php echo css_content_div; ?>">
-</div>
-</body>
+	<?php print_head("Главная страница");?>
+	<body>
+		<?php	print_header();?>
+		<div id = "vmenu">
+		<?php	print_login_form();?>
+		</div>
+		<div id = "<?php echo css_content_div; ?>">
+			<div id = "main_news">
+			<table id = "news_table">
+			<?php
+				if (pg_num_rows($res) == 0) {
+					write_user_message("Ресурс не добавлен");
+				} else {
+					$fmt = "<tr>
+						<td class = 'news_head'>%s</td>
+						<td class = 'new_day'>%s</td>
+						</tr>
+						<tr class = 'desc'>
+						<td colspan=2 class = 'news_des'>%s</td>
+						</tr>";
+					while ($row = pg_fetch_assoc($res)) {
+						printf ($fmt,
+							$row['news_header'], 
+							date("d.m.Y H:i", $row['news_date']), 
+							$row['news_text']
+							);
+						$i++;
+					}			
+				}
+			?>
+			</table>
+			</div>		
+			<div id = "main_lib">
+				<p class = "tit">Новые книги</p>
+				Макс, сюда списком нужно вывести три самые свежие книги 
+				из твоей библиотеки :)
+				(И не спрашивай, почему я не сделала это сама!)
+			</div>
+		</div>
+	</body>
 </html>
--- trunk/sql/00-disciplines.sql
+++ trunk/sql/00-disciplines.sql
@@ -11,6 +11,7 @@ CREATE TABLE disciplines (
 	labs INTEGER NOT NULL,
 	courseovik VARCHAR,
 	control VARCHAR,
+	about VARCHAR,
 	prof BOOLEAN NOT NULL DEFAULT FALSE,
 	UNIQUE(name),
 	CHECK(lessons > -1),
@@ -27,6 +28,7 @@ SELECT
 	practices AS disc_practices,
 	labs AS disc_labs,
 	courseovik AS disc_courseovik,
+	about AS disc_about,
 	prof AS isprof
 FROM
 	disciplines;
@@ -61,18 +63,20 @@ DROP FUNCTION IF EXISTS ADDDISC(IN disciplines.name%TYPE,
 				IN disciplines.practices%TYPE,
 				IN disciplines.labs%TYPE,
 				IN disciplines.courseovik%TYPE,
-				IN disciplines.control%TYPE);
+				IN disciplines.control%TYPE,
+				IN disciplines.about%TYPE);
 CREATE FUNCTION ADDDISC(IN disciplines.name%TYPE,
 			IN INTEGER[],
 			IN disciplines.lessons%TYPE,
 			IN disciplines.practices%TYPE,
 			IN disciplines.labs%TYPE,
 			IN disciplines.courseovik%TYPE,
-			IN disciplines.control%TYPE)
+			IN disciplines.control%TYPE,
+			IN disciplines.about%TYPE)
 RETURNS VOID AS $$
 BEGIN
-	INSERT INTO disciplines(name, lessons, practices, labs, courseovik, control)
-	VALUES($1, $3, $4, $5, $6, $7);
+	INSERT INTO disciplines(name, lessons, practices, labs, courseovik, control, about)
+	VALUES($1, $3, $4, $5, $6, $7, $8);
 
 	IF $2 IS NOT NULL THEN
 		FOR i IN array_lower($2, 1) .. array_upper($2, 1) LOOP
@@ -92,17 +96,19 @@ DROP FUNCTION IF EXISTS ADD_PROFDISC(IN disciplines.name%TYPE,
 				IN disciplines.practices%TYPE,
 				IN disciplines.labs%TYPE,
 				IN disciplines.courseovik%TYPE,
-				IN disciplines.control%TYPE);
+				IN disciplines.control%TYPE,
+				IN disciplines.about%TYPE);
 CREATE FUNCTION ADD_PROFDISC(IN disciplines.name%TYPE,
 			IN INTEGER[],
 			IN disciplines.lessons%TYPE,
 			IN disciplines.practices%TYPE,
 			IN disciplines.labs%TYPE,
 			IN disciplines.courseovik%TYPE,
-			IN disciplines.control%TYPE)
+			IN disciplines.control%TYPE,
+			IN disciplines.about%TYPE)
 RETURNS VOID AS $$
 BEGIN
-	PERFORM ADDDISC($1, $2, $3, $4, $5, $6, $7);
+	PERFORM ADDDISC($1, $2, $3, $4, $5, $6, $7, $8);
 	UPDATE disciplines SET prof = TRUE WHERE name = $1;
 END;
 $$ LANGUAGE plPGSQL;
@@ -112,4 +118,30 @@ $$ LANGUAGE plPGSQL;
 --
 --
 
-SELECT ADD_PROFDISC('Математический анализ', ARRAY[1,2], 0, 0, 0, 'Поверхности', 'Экзамен');
+SELECT ADD_PROFDISC('Математический анализ', ARRAY[1,2], 0, 0, 0, 'есть', 'Экзамен','');
+SELECT ADD_PROFDISC('Линейная алгебра и аналитическая геометрия', ARRAY[1,2], 0, 0, 0, 'нет' ,'Экзамен','');
+SELECT ADD_PROFDISC('Дискретная математика', ARRAY[1], 0, 0, 0, 'нет','Экзамен','');
+SELECT ADD_PROFDISC('Теория вероятностей и математическая статистика', ARRAY[2], 0, 0, 0, 'есть', 'Экзамен','');
+SELECT ADD_PROFDISC('Теория случайных процессов', ARRAY[3], 0, 0, 0, '', 'Экзамен','');
+SELECT ADD_PROFDISC('Теория функций комплексного переменного', ARRAY[2], 0, 0, 0, 'нет', 'Экзамен','');
+SELECT ADD_PROFDISC('Дифференциальные уравнения', ARRAY[2], 0, 0, 0, 'нет', 'Экзамен','');
+SELECT ADD_PROFDISC('Вариационное исчисление', ARRAY[3], 0, 0, 0, 'есть', 'Зачет','');
+SELECT ADD_PROFDISC('Уравнения в частных производных', ARRAY[3], 0, 0, 0, 'нет', 'Экзамен','');
+SELECT ADD_PROFDISC('Математическое моделирование', ARRAY[3,4], 0, 0, 0, 'есть', 'Экзамен','');
+SELECT ADD_PROFDISC('Алгоритмические языки и программирование', ARRAY[1,2], 0, 0, 0, 'есть', 'Экзамен','');
+SELECT ADD_PROFDISC('Базы данных', ARRAY[5], 0, 0, 0, 'есть', 'Экзамен','');
+SELECT ADD_PROFDISC('Компьютерная графика', ARRAY[3], 0, 0, 0, 'есть', 'Экзамен','');
+SELECT ADD_PROFDISC('Алгоритмика', ARRAY[4], 0, 0, 0, 'нет', 'Зачет','');
+SELECT ADD_PROFDISC('Архитектура ЭВМ', ARRAY[2,3], 0, 0, 0, 'нет', 'Экзамен','');
+SELECT ADD_PROFDISC('Основы теории информационных сетей', ARRAY[5], 0, 0, 0, 'нет', 'Зачет','');
+SELECT ADD_PROFDISC('Теория оптимизации', ARRAY[3], 0, 0, 0, 'нет', 'Зачет','');
+SELECT ADD_PROFDISC('Численные методы', ARRAY[3], 0, 0, 0, 'нет', 'Экзамен','');
+SELECT ADD_PROFDISC('Теория управления', ARRAY[5], 0, 0, 0, 'нет', 'Экзамен','');
+SELECT ADD_PROFDISC('Теория игр', ARRAY[4], 0, 0, 0, 'нет', 'Экзамен','');
+SELECT ADD_PROFDISC('Интеллектуальные системы', ARRAY[5], 0, 0, 0, 'нет', 'Экзамен','');
+SELECT ADD_PROFDISC('Математические модели сигналов', ARRAY[3], 0, 0, 0, 'нет', 'Зачет','');
+SELECT ADD_PROFDISC('Функциональный анализ', ARRAY[3], 0, 0, 0, 'нет', 'Экзамен','');
+SELECT ADD_PROFDISC('Основы теории систем', ARRAY[4], 0, 0, 0, 'нет', 'Экзамен','');
+SELECT ADD_PROFDISC('Методы и средства визуального проектирования', ARRAY[3], 0, 0, 0, 'нет', 'Зачет','');
+SELECT ADD_PROFDISC('Программное обеспечение систем обмена данными', ARRAY[4], 0, 0, 0, '', 'Экзамен','');
+
