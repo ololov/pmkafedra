cde981bd6d3249c9d4e8ac2ec529884e469beb30
fixer21k <fixer21k@4ef296e0-c894-11de-8f3f-2f730f80caef>
2010-03-04 09:11:59 +0000

    Сделал вывод новых книг на главной.

--- index.php
+++ index.php
@@ -9,12 +9,26 @@
 	if (!$link)
 		include_once('include/html_db_error.php');
 
-	$sql = "SELECT news_text, news_header, CAST(news_date AS DATE) ".
-	       "FROM news_tb ORDER BY news_date LIMIT 3;";
+	$sql = "SELECT news_text, news_header, news_date ".
+	       "FROM news_tb ORDER BY news_date DESC LIMIT 3;";
 
 	$res = pg_query($link, $sql);
 	if (!$res)
 		include_once('include/html_db_error.php');
+
+	$sql = "SELECT book_id, CAST(book_posted AS DATE), book_name, book_who ".
+	       "FROM books_tb ORDER BY book_posted LIMIT 3;";
+	$bres = pg_query($link, $sql);
+	if (!$bres)
+		include_once('include/html_db_error.php');
+
+	$fmt = "<tr>" .
+		"<td class = 'news_head'>%s</td>".
+		"<td class = 'new_day'>%s</td>".
+		"</tr>".
+		"<tr class = 'desc'>".
+		"<td colspan=2 class = 'news_des'>%s</td>".
+		"</tr>";
 ?>
 
 <!DOCTYPE html>
@@ -32,20 +46,12 @@
 				if (pg_num_rows($res) == 0) {
 					write_user_message("Ресурс не добавлен");
 				} else {
-					$fmt = "<tr>
-						<td class = 'news_head'>%s</td>
-						<td class = 'new_day'>%s</td>
-						</tr>
-						<tr class = 'desc'>
-						<td colspan=2 class = 'news_des'>%s</td>
-						</tr>";
 					while ($row = pg_fetch_assoc($res)) {
 						printf ($fmt,
 							$row['news_header'], 
 							$row['news_date'], 
 							$row['news_text']
 							);
-						$i++;
 					}			
 				}
 			?>
@@ -53,9 +59,22 @@
 			</div>		
 			<div id = "main_lib">
 				<p class = "tit">Новые книги</p>
-				Макс, сюда списком нужно вывести три самые свежие книги 
-				из твоей библиотеки :)
-				(И не спрашивай, почему я не сделала это сама!)
+				<div id = "main_news">
+				<table id = "news_table">
+<?php
+	while (($row = pg_fetch_assoc($bres))) {
+		$bid = $row['book_id'];
+		printf("<tr><td>%s</td><td>%s</td><td>%s</td></tr>",
+			$row['book_posted'],
+			sprintf("<a url = \"%s\">%s</a>",
+				lib_url . htmlspecialchars("/desc.php?book_id=$bid"),
+				$row['book_name']),
+			$row['book_who']);
+	}
+?>
+
+			</table>
+			</div>		
 			</div>
 		</div>
 	</body>
